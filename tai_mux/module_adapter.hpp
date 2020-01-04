bc0ec039782cf82508a3e8804fd3fc4bb39e1696
Wataru Ishida <ishida@nel-america.com>
2020-01-04 17:43:04 +0000

    mux: overhaul by using TAI library framework
    Signed-off-by: Wataru Ishida <ishida@nel-america.com>

--- tai_mux/module_adapter.hpp
+++ tai_mux/module_adapter.hpp
@@ -1,181 +1,211 @@
 #ifndef __MODULE_ADAPTER_HPP__
 #define __MODULE_ADAPTER_HPP__
 
-#include <string>
 #include "tai.h"
+#include <string>
+#include <memory>
 
-typedef tai_status_t (*tai_api_initialize_fn) (uint64_t, const tai_service_method_table_t *);
-typedef tai_status_t (*tai_api_uninitialize_fn) (void);
-typedef tai_status_t (*tai_api_query_fn) (tai_api_t, void**);
-typedef tai_status_t (*tai_log_set_fn) (tai_api_t, tai_log_level_t, tai_log_fn);
-typedef tai_object_type_t (*tai_object_type_query_fn) (tai_object_id_t);
-typedef tai_object_id_t (*tai_module_id_query_fn) (tai_object_id_t);
+namespace tai::mux {
 
-// corresponds to one dynamic library
-class ModuleAdapter {
-    public:
-        ModuleAdapter(const std::string& name, uint64_t flags, const tai_service_method_table_t* services);
-        ~ModuleAdapter();
-        static uint64_t dl_address(const std::string& name);
+    typedef tai_status_t (*tai_api_initialize_fn) (uint64_t, const tai_service_method_table_t *);
+    typedef tai_status_t (*tai_api_uninitialize_fn) (void);
+    typedef tai_status_t (*tai_api_query_fn) (tai_api_t, void**);
+    typedef tai_status_t (*tai_log_set_fn) (tai_api_t, tai_log_level_t, tai_log_fn);
+    typedef tai_object_type_t (*tai_object_type_query_fn) (tai_object_id_t);
+    typedef tai_object_id_t (*tai_module_id_query_fn) (tai_object_id_t);
 
-        tai_status_t tai_api_initialize(uint64_t flags, const tai_service_method_table_t* services) {
-            return m_tai_api_initialize(flags, services);
-        }
-        tai_status_t tai_api_query(tai_api_t tai_api_id, void** api_method_table) {
-            return m_tai_api_query(tai_api_id, api_method_table);
-        }
-        tai_status_t tai_api_uninitialize(void) {
-            return m_tai_api_uninitialize();
-        }
-        tai_status_t tai_log_set(tai_api_t tai_api_id, tai_log_level_t log_level, tai_log_fn log_fn) {
-            return m_tai_log_set(tai_api_id, log_level, log_fn);
-        }
-        tai_object_type_t tai_object_type_query(tai_object_id_t tai_object_id) {
-            return m_tai_object_type_query(tai_object_id);
-        }
-        tai_object_id_t tai_module_id_query(tai_object_id_t tai_object_id) {
-            return m_tai_module_id_query(tai_object_id);
-        }
+    // corresponds to one dynamic library
+    class ModuleAdapter {
+        public:
+            ModuleAdapter(const std::string& name, uint64_t flags, const tai_service_method_table_t* services);
+            ~ModuleAdapter();
+            static uint64_t dl_address(const std::string& name);
 
-        tai_status_t create_module(
-            _Out_ tai_object_id_t          *module_id,
-            _In_ uint32_t                   attr_count,
-            _In_ const tai_attribute_t     *attr_list) {
-            if ( m_module_api == nullptr || m_module_api->create_module == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t tai_api_initialize(uint64_t flags, const tai_service_method_table_t* services) {
+                return m_tai_api_initialize(flags, services);
             }
-            return m_module_api->create_module(module_id, attr_count, attr_list);
-        }
-
-        tai_status_t remove_module(
-            _In_ tai_object_id_t module_id) {
-            if ( m_module_api == nullptr || m_module_api->remove_module == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t tai_api_query(tai_api_t tai_api_id, void** api_method_table) {
+                return m_tai_api_query(tai_api_id, api_method_table);
             }
-            return m_module_api->remove_module(module_id);
-        }
-
-        tai_status_t set_module_attributes(
-            _In_ tai_object_id_t module_id,
-            _In_ uint32_t attr_count,
-            _In_ const tai_attribute_t *attr_list) {
-            if ( m_module_api == nullptr || m_module_api->set_module_attributes == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t tai_api_uninitialize(void) {
+                return m_tai_api_uninitialize();
             }
-            return m_module_api->set_module_attributes(module_id, attr_count, attr_list);
-        }
-
-        tai_status_t get_module_attributes(
-            _In_ tai_object_id_t module_id,
-            _In_ uint32_t attr_count,
-            _In_ tai_attribute_t *attr_list) {
-            if ( m_module_api == nullptr || m_module_api->get_module_attributes == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t tai_log_set(tai_api_t tai_api_id, tai_log_level_t log_level, tai_log_fn log_fn) {
+                return m_tai_log_set(tai_api_id, log_level, log_fn);
             }
-            return m_module_api->get_module_attributes(module_id, attr_count, attr_list);
-        }
-
-        tai_status_t create_network_interface(
-            _Out_ tai_object_id_t *network_interface_id,
-            _In_ tai_object_id_t module_id,
-            _In_ uint32_t attr_count,
-            _In_ const tai_attribute_t *attr_list) {
-            if ( m_netif_api == nullptr || m_netif_api->create_network_interface == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_object_type_t tai_object_type_query(tai_object_id_t tai_object_id) {
+                return m_tai_object_type_query(tai_object_id);
             }
-            return m_netif_api->create_network_interface(network_interface_id, module_id, attr_count, attr_list);
-        }
-
-        tai_status_t remove_network_interface(
-            _In_ tai_object_id_t network_interface_id) {
-            if ( m_netif_api == nullptr || m_netif_api->remove_network_interface == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_object_id_t tai_module_id_query(tai_object_id_t tai_object_id) {
+                return m_tai_module_id_query(tai_object_id);
             }
-            return m_netif_api->remove_network_interface(network_interface_id);
-        }
 
-        tai_status_t set_network_interface_attributes(
-            _In_ tai_object_id_t network_interface_id,
-            _In_ uint32_t attr_count,
-            _In_ const tai_attribute_t *attr_list) {
-            if ( m_netif_api == nullptr || m_netif_api->set_network_interface_attributes == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t set_attributes(tai_object_type_t type, tai_object_id_t oid, uint32_t count, const tai_attribute_t *list) {
+                switch (type) {
+                case TAI_OBJECT_TYPE_MODULE:
+                    return set_module_attributes(oid, count, list);
+                case TAI_OBJECT_TYPE_NETWORKIF:
+                    return set_network_interface_attributes(oid, count, list);
+                case TAI_OBJECT_TYPE_HOSTIF:
+                    return set_host_interface_attributes(oid, count, list);
+                }
+                return TAI_STATUS_NOT_SUPPORTED;
             }
-            return m_netif_api->set_network_interface_attributes(network_interface_id, attr_count, attr_list);
-        }
 
-        tai_status_t get_network_interface_attributes(
-            _In_ tai_object_id_t network_interface_id,
-            _In_ uint32_t attr_count,
-            _In_ tai_attribute_t *attr_list) {
-            if ( m_netif_api == nullptr || m_netif_api->get_network_interface_attributes == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t get_attributes(tai_object_type_t type, tai_object_id_t oid, uint32_t count, tai_attribute_t *list) {
+                switch (type) {
+                case TAI_OBJECT_TYPE_MODULE:
+                    return get_module_attributes(oid, count, list);
+                case TAI_OBJECT_TYPE_NETWORKIF:
+                    return get_network_interface_attributes(oid, count, list);
+                case TAI_OBJECT_TYPE_HOSTIF:
+                    return get_host_interface_attributes(oid, count, list);
+                }
+                return TAI_STATUS_NOT_SUPPORTED;
             }
-            return m_netif_api->get_network_interface_attributes(network_interface_id, attr_count, attr_list);
-        }
 
-        tai_status_t create_host_interface(
-            _Out_ tai_object_id_t *host_interface_id,
-            _In_ tai_object_id_t module_id,
-            _In_ uint32_t attr_count,
-            _In_ const tai_attribute_t *attr_list) {
-            if ( m_hostif_api == nullptr || m_hostif_api->create_host_interface == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t create_module(
+                _Out_ tai_object_id_t          *module_id,
+                _In_ uint32_t                   attr_count,
+                _In_ const tai_attribute_t     *attr_list) {
+                if ( m_module_api == nullptr || m_module_api->create_module == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_module_api->create_module(module_id, attr_count, attr_list);
             }
-            return m_hostif_api->create_host_interface(host_interface_id, module_id, attr_count, attr_list);
-        }
 
-        tai_status_t remove_host_interface(
-            _In_ tai_object_id_t host_interface_id) {
-            if ( m_hostif_api == nullptr || m_hostif_api->remove_host_interface == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t remove_module(
+                _In_ tai_object_id_t module_id) {
+                if ( m_module_api == nullptr || m_module_api->remove_module == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_module_api->remove_module(module_id);
             }
-            return m_hostif_api->remove_host_interface(host_interface_id);
-        }
 
-        tai_status_t set_host_interface_attributes(
-            _In_ tai_object_id_t host_interface_id,
-            _In_ uint32_t attr_count,
-            _In_ const tai_attribute_t *attr_list) {
-            if ( m_hostif_api == nullptr || m_hostif_api->set_host_interface_attributes == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t set_module_attributes(
+                _In_ tai_object_id_t module_id,
+                _In_ uint32_t attr_count,
+                _In_ const tai_attribute_t *attr_list) {
+                if ( m_module_api == nullptr || m_module_api->set_module_attributes == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_module_api->set_module_attributes(module_id, attr_count, attr_list);
             }
-            return m_hostif_api->set_host_interface_attributes(host_interface_id, attr_count, attr_list);
-        }
 
-        tai_status_t get_host_interface_attributes(
-            _In_ tai_object_id_t host_interface_id,
-            _In_ uint32_t attr_count,
-            _In_ tai_attribute_t *attr_list) {
-            if ( m_hostif_api == nullptr || m_hostif_api->get_host_interface_attributes == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t get_module_attributes(
+                _In_ tai_object_id_t module_id,
+                _In_ uint32_t attr_count,
+                _In_ tai_attribute_t *attr_list) {
+                if ( m_module_api == nullptr || m_module_api->get_module_attributes == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_module_api->get_module_attributes(module_id, attr_count, attr_list);
             }
-            return m_hostif_api->get_host_interface_attributes(host_interface_id, attr_count, attr_list);
-        }
 
-        tai_status_t clear_host_interface_attributes(
-            _In_ tai_object_id_t host_interface_id,
-            _In_ uint32_t attr_count,
-            _In_ tai_attr_id_t *attr_list) {
-            if ( m_hostif_api == nullptr || m_hostif_api->clear_host_interface_attributes == nullptr ) {
-                return TAI_STATUS_FAILURE;
+            tai_status_t create_network_interface(
+                _Out_ tai_object_id_t *network_interface_id,
+                _In_ tai_object_id_t module_id,
+                _In_ uint32_t attr_count,
+                _In_ const tai_attribute_t *attr_list) {
+                if ( m_netif_api == nullptr || m_netif_api->create_network_interface == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_netif_api->create_network_interface(network_interface_id, module_id, attr_count, attr_list);
             }
-            return m_hostif_api->clear_host_interface_attributes(host_interface_id, attr_count, attr_list);
-        }
 
-    private:
-        void* m_dl;
-        const std::string& m_name;
-        tai_api_initialize_fn    m_tai_api_initialize;
-        tai_api_uninitialize_fn  m_tai_api_uninitialize;
-        tai_api_query_fn         m_tai_api_query;
-        tai_log_set_fn           m_tai_log_set;
-        tai_object_type_query_fn m_tai_object_type_query;
-        tai_module_id_query_fn   m_tai_module_id_query;
+            tai_status_t remove_network_interface(
+                _In_ tai_object_id_t network_interface_id) {
+                if ( m_netif_api == nullptr || m_netif_api->remove_network_interface == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_netif_api->remove_network_interface(network_interface_id);
+            }
 
-        tai_module_api_t*            m_module_api;
-        tai_host_interface_api_t*    m_hostif_api;
-        tai_network_interface_api_t* m_netif_api;
+            tai_status_t set_network_interface_attributes(
+                _In_ tai_object_id_t network_interface_id,
+                _In_ uint32_t attr_count,
+                _In_ const tai_attribute_t *attr_list) {
+                if ( m_netif_api == nullptr || m_netif_api->set_network_interface_attributes == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_netif_api->set_network_interface_attributes(network_interface_id, attr_count, attr_list);
+            }
+
+            tai_status_t get_network_interface_attributes(
+                _In_ tai_object_id_t network_interface_id,
+                _In_ uint32_t attr_count,
+                _In_ tai_attribute_t *attr_list) {
+                if ( m_netif_api == nullptr || m_netif_api->get_network_interface_attributes == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_netif_api->get_network_interface_attributes(network_interface_id, attr_count, attr_list);
+            }
+
+            tai_status_t create_host_interface(
+                _Out_ tai_object_id_t *host_interface_id,
+                _In_ tai_object_id_t module_id,
+                _In_ uint32_t attr_count,
+                _In_ const tai_attribute_t *attr_list) {
+                if ( m_hostif_api == nullptr || m_hostif_api->create_host_interface == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_hostif_api->create_host_interface(host_interface_id, module_id, attr_count, attr_list);
+            }
+
+            tai_status_t remove_host_interface(
+                _In_ tai_object_id_t host_interface_id) {
+                if ( m_hostif_api == nullptr || m_hostif_api->remove_host_interface == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_hostif_api->remove_host_interface(host_interface_id);
+            }
+
+            tai_status_t set_host_interface_attributes(
+                _In_ tai_object_id_t host_interface_id,
+                _In_ uint32_t attr_count,
+                _In_ const tai_attribute_t *attr_list) {
+                if ( m_hostif_api == nullptr || m_hostif_api->set_host_interface_attributes == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_hostif_api->set_host_interface_attributes(host_interface_id, attr_count, attr_list);
+            }
+
+            tai_status_t get_host_interface_attributes(
+                _In_ tai_object_id_t host_interface_id,
+                _In_ uint32_t attr_count,
+                _In_ tai_attribute_t *attr_list) {
+                if ( m_hostif_api == nullptr || m_hostif_api->get_host_interface_attributes == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_hostif_api->get_host_interface_attributes(host_interface_id, attr_count, attr_list);
+            }
+
+            tai_status_t clear_host_interface_attributes(
+                _In_ tai_object_id_t host_interface_id,
+                _In_ uint32_t attr_count,
+                _In_ tai_attr_id_t *attr_list) {
+                if ( m_hostif_api == nullptr || m_hostif_api->clear_host_interface_attributes == nullptr ) {
+                    return TAI_STATUS_FAILURE;
+                }
+                return m_hostif_api->clear_host_interface_attributes(host_interface_id, attr_count, attr_list);
+            }
+
+        private:
+            void* m_dl;
+            const std::string& m_name;
+            tai_api_initialize_fn    m_tai_api_initialize;
+            tai_api_uninitialize_fn  m_tai_api_uninitialize;
+            tai_api_query_fn         m_tai_api_query;
+            tai_log_set_fn           m_tai_log_set;
+            tai_object_type_query_fn m_tai_object_type_query;
+            tai_module_id_query_fn   m_tai_module_id_query;
+
+            tai_module_api_t*            m_module_api;
+            tai_host_interface_api_t*    m_hostif_api;
+            tai_network_interface_api_t* m_netif_api;
+    };
+
+    using S_ModuleAdapter = std::shared_ptr<ModuleAdapter>;
+
 };
-
 #endif
--- tai_mux/platform_adapter.hpp
+++ tai_mux/platform_adapter.hpp
@@ -3,24 +3,126 @@
 
 #include <string>
 #include <unordered_set>
+#include <memory>
+#include <bitset>
+#include <map>
+#include <mutex>
 
-class ModuleAdapter;
+#include "tai.h"
+#include "attribute.hpp"
+#include "logger.hpp"
 
-enum platform_adapter_t {
-    PLATFORM_ADAPTER_UNKNOWN,
-    PLATFORM_ADAPTER_STATIC,
-};
+namespace tai::mux {
 
-class PlatformAdapter {
-    public:
-        virtual ModuleAdapter* get_module_adapter(const std::string& location) = 0;
+    class ModuleAdapter;
+    using S_ModuleAdapter = std::shared_ptr<ModuleAdapter>;
 
-        /** @brief return the set of loaded module adapters. */
-        virtual const std::unordered_set<ModuleAdapter * > list_module_adapters() = 0;
-        PlatformAdapter(){}
-    private:
-        PlatformAdapter(const PlatformAdapter&){}
-        void operator = (const PlatformAdapter&){}
+    class PlatformAdapter;
+    using S_PlatformAdapter = std::shared_ptr<PlatformAdapter>;
+
+    using notification_key = std::pair<tai_object_id_t, tai_attr_id_t>;
+
+    struct NotificationContext {
+        PlatformAdapter *pa;
+        tai_notification_handler_t real_handler;
+        tai_object_id_t muxed_oid;
+        tai_object_type_t object_type;
+        std::mutex mutex;
+    };
+
+    using S_NotificationContext = std::shared_ptr<NotificationContext>;
+
+    enum platform_adapter_t {
+        PLATFORM_ADAPTER_UNKNOWN,
+        PLATFORM_ADAPTER_STATIC,
+    };
+
+    static const int TAI_MUX_NUM_MAX_OBJECT = 256;
+
+    class OIDAllocator {
+        public:
+            tai_object_id_t next() {
+                // TAI_NULL_OBJECT_ID == 0, start from i = 1
+                for (int i = 1; i < TAI_MUX_NUM_MAX_OBJECT; i++) {
+                    if ( !m_bitset.test(i) ) {
+                        m_bitset.set(i);
+                        return tai_object_id_t(i);
+                    }
+                }
+                throw std::runtime_error("OID max reach");
+            }
+            void free(tai_object_id_t oid) {
+                m_bitset.reset(oid);
+            }
+        private:
+            std::bitset<TAI_MUX_NUM_MAX_OBJECT> m_bitset;
+    };
+
+    class PlatformAdapter {
+        public:
+            virtual S_ModuleAdapter get_module_adapter(const std::string& location) = 0;
+
+            /** @brief return the set of loaded module adapters. */
+            virtual const std::unordered_set<S_ModuleAdapter> list_module_adapters() = 0;
+            PlatformAdapter(){}
+            virtual ~PlatformAdapter(){}
+
+            int get_mapping(const tai_object_id_t& id, S_ModuleAdapter *adapter, tai_object_id_t *real_id) {
+                if ( m_map.find(id) == m_map.end() ) {
+                    return -1;
+                }
+                auto pair = m_map[id];
+                if ( adapter != nullptr ) {
+                    *adapter = pair.second;
+                }
+                if ( real_id != nullptr ) {
+                    *real_id = pair.first;
+                }
+                return 0;
+            };
+
+            tai_object_id_t get_reverse_mapping(const tai_object_id_t real_id, S_ModuleAdapter adapter) {
+                for ( auto p : m_map ) {
+                    if ( p.second.first == real_id && p.second.second == adapter ) {
+                        return p.first;
+                    }
+                }
+                return TAI_NULL_OBJECT_ID;
+            }
+
+            int create_mapping(tai_object_id_t *id, S_ModuleAdapter adapter, const tai_object_id_t& real_id) {
+                auto value = std::pair<tai_object_id_t, S_ModuleAdapter>(real_id, adapter);
+                *id = m_oid_allocator.next();
+                m_map[*id] = value;
+                return 0;
+            }
+
+            virtual int remove_mapping(tai_object_id_t id) {
+                if ( m_map.find(id) == m_map.end() ) {
+                    return 0;
+                }
+                m_map.erase(id);
+                m_oid_allocator.free(id);
+                return 0;
+            }
+
+            void notify(NotificationContext* ctx, tai_object_id_t real_oid, uint32_t attr_count, tai_attribute_t const * const attr_list);
+
+            tai_status_t convert_oid(const tai_object_type_t& type, const tai_object_id_t& id, const S_ConstAttribute src, const S_Attribute dst, bool reversed);
+            tai_status_t convert_oid(const tai_object_type_t& type, const tai_object_id_t& id, const tai_attribute_t * const src, tai_attribute_t * const dst, bool reversed);
+
+            tai_status_t set(const tai_object_type_t& type, const tai_object_id_t& id, const tai_attribute_t* const attribute);
+
+        private:
+            PlatformAdapter(const PlatformAdapter&){}
+            void operator = (const PlatformAdapter&){}
+            OIDAllocator m_oid_allocator;
+            std::map<tai_object_id_t, std::pair<tai_object_id_t, S_ModuleAdapter>> m_map;
+            std::map<notification_key, S_NotificationContext> m_notification_map;
+    };
+
+    using S_PlatformAdapter = std::shared_ptr<PlatformAdapter>;
+
 };
 
 #endif
