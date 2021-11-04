ccb92bd72f2244c3183f14fd7cbbab0b2f48b264
Wataru Ishida <wataru.ishid@gmail.com>
2021-11-04 01:47:40 +0000

    mux: add tests for exec platform adapter
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

---
+++ tai_mux/tests/exec.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+import time
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    arg = sys.argv[1]
+
+    if arg == "list":
+        print("\n".join(str(v) for v in range(1, 8)))
+        return
+
+    time.sleep(.5)
+
+    print("libtai-a.so")
+
+
+if __name__ == "__main__":
+    main()
